c1eff546c347c827b06277022f9f0806fb8e53ca
Sri Rizki Batu Bara <sririzki.02062000@gmail.com>
2021-10-22 11:54:06 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,37 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Sri Rizki Batu Bara", 21, 165);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+  
+      // contoh:
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+      hero.makan();
+      hero.makan();
+
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+
+      hero.duduk();
+
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
